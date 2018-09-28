0ebfdf2281dab7ff217f1ae9010baafdac75a2ba
Koval Nastya <anonymusua99@gmail.com>
2018-09-28 14:54:59 +0300

    Upd

--- Project1/Currency.cs
+++ Project1/Currency.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Project1
 {
-    public class Currency
+    public enum Koef {DOL, UAH,EUR }
+    public interface IRead {
+        void Read(string name);
+    }
+    public class Currency:IRead
     {
         public int Ammount { get; set; }
         public string CurrencyName { get; set; }
-
+       public  List<Currency> CurrencyList = new List<Currency>();
         public Currency()
         {
-            this.Ammount = 0;
-            this.CurrencyName = "currency_name";
+            this.Ammount = 10;
+            this.CurrencyName = "dolar";
         }
 
         public Currency(int ammnt, string cur_name)
@@ -29,12 +34,44 @@ namespace Project1
             Console.WriteLine("Ammount:       {0}", this.Ammount);
             Console.WriteLine();
         }
-
+        public void ConvertTo(string val)
+        {
+            
+        }
         public override string ToString()
         {
             return String.Format("Currency: \nAmmount: {0}\nName:    {1}\n",
                 this.Ammount, this.CurrencyName);
         }
+        public void Read(string name)
+        {
+           
+            using (StreamReader str = new StreamReader(name,
+                                    Encoding.Default))
+            {
+                while (!str.EndOfStream)
+                {
+                    string current_currency = str.ReadLine();
+                    CurrencyList.Add(new Currency(int.Parse(current_currency.Split(new char[] { ' ' })[0]),
+                                        current_currency.Split(new char[] { ' ' })[1]));
+                }
+                foreach (var c in CurrencyList)
+                {
+                    if(c.CurrencyName=="hrivnia")
+                    Console.WriteLine(c);
+                }
+            }
+        }
+        public void ConvertToDictionary()
+        {
+            Dictionary<string, int> d = new Dictionary<string, int>();
 
+            for (int i = 0; i < CurrencyList.Count; i++)
+            {
+
+
+                d.Add(CurrencyList[i].CurrencyName,CurrencyList[i].Ammount);
+            }
+        }
     }
 }
--- Project1/Program.cs
+++ Project1/Program.cs
@@ -11,33 +11,14 @@ namespace Project1
 {
     class Program
     {
-        interface IReadable
-        {
-            List<int> Read();  //List<Current>
-        }
 
         static void Main(string[] args)
         {
             Currency Cur1 = new Currency(3444, "Hrivnia");
             Cur1.Print();
 
-            List<Currency> CurrencyList = new List<Currency>();
+            Cur1.Read("currencies.txt");
 
-            using (StreamReader str = new StreamReader("currencies.txt",
-                                    Encoding.Default))
-            {
-                while (!str.EndOfStream)
-                {
-                    string current_currency = str.ReadLine();
-                    CurrencyList.Add(new Currency(int.Parse(current_currency.Split(new char[] { ' ' })[0]),
-                        current_currency.Split(new char[] { ' ' })[1]));
-                }
-            }
-
-            foreach (Currency p in CurrencyList)
-            {
-                Console.WriteLine(p);
-            }
 
             Console.ReadKey();
         }
