238434d282d4c0008d5f87eec9cfb27f7592ec1f
Koval Nastya <anonymusua99@gmail.com>
2018-09-28 13:48:33 +0300

    update

---
+++ Project1/Currency.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    public class Currency
+    {
+        public int Ammount { get; set; }
+        public string CurrencyName { get; set; }
+
+        public Currency()
+        {
+            this.Ammount = 0;
+            this.CurrencyName = "currency_name";
+        }
+
+        public Currency(int ammnt, string cur_name)
+        {
+            this.Ammount = ammnt;
+            this.CurrencyName = cur_name;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Currency name: {0}", this.CurrencyName);
+            Console.WriteLine("Ammount:       {0}", this.Ammount);
+            Console.WriteLine();
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Currency: \nAmmount: {0}\nName:    {1}\n",
+                this.Ammount, this.CurrencyName);
+        }
+
+    }
+}
---
+++ Project1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+
+namespace Project1
+{
+    class Program
+    {
+        interface IReadable
+        {
+            List<int> Read();  //List<Current>
+        }
+
+        static void Main(string[] args)
+        {
+            Currency Cur1 = new Currency(3444, "Hrivnia");
+            Cur1.Print();
+
+            List<Currency> CurrencyList = new List<Currency>();
+
+            using (StreamReader str = new StreamReader("currencies.txt",
+                                    Encoding.Default))
+            {
+                while (!str.EndOfStream)
+                {
+                    string current_currency = str.ReadLine();
+                    CurrencyList.Add(new Currency(int.Parse(current_currency.Split(new char[] { ' ' })[0]),
+                        current_currency.Split(new char[] { ' ' })[1]));
+                }
+            }
+
+            foreach (Currency p in CurrencyList)
+            {
+                Console.WriteLine(p);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
